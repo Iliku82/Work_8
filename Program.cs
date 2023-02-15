3537522893f75a7f213466d8aa75358b34c6a0e2
Petr <petrkiseev82@mail.ru>
2023-02-15 03:42:19 +0300

    Papca_Work

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+SortArray(array);
+Console.WriteLine();
+Printarray(array);
+
+
+// ------------------м--------------
+
+void SortArray (int [,] array)
+{
+    for (int i = 0; i<array.GetLength(0); i++)
+    {
+        for (int j = 0; j<array.GetLength(1); j++)
+        {
+            for (int t = j+1; t<array.GetLength(1);t++)
+            {
+                if (array[i,t]>array[i,j])
+                {
+                  int temp = array[i,j];
+                  array[i,j]=array[i,t];
+                  array[i,t] = temp;
+                }
+            }
+        }
+    }
+}
